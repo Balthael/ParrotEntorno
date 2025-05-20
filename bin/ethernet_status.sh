5c97edf361393219ade3c319f7c14d22e00d552b
BalthaEl <119884930+Balthael@users.noreply.github.com>
2025-05-19 21:07:53 -0500

    Update ethernet_status.sh

--- bin/ethernet_status.sh
+++ bin/ethernet_status.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-echo "%{F#2495e7}󰈀 %{F#ffffff}$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')%{u-}"
+# Detectar interfaces activas con IP (exceptuando loopback)
+ACTIVE_IFACES=$(ip -o -4 addr show up | awk '!/ lo / {print $2}')
+
+# Inicializar IP vacía
+IP="Sin conexión"
+
+# Recorrer interfaces activas
+for IFACE in $ACTIVE_IFACES; do
+    CUR_IP=$(ip -o -4 addr show $IFACE | awk '{print $4}' | cut -d/ -f1)
+    if echo "$IFACE" | grep -qE '^wl'; then
+        ICON=""  # Wi-Fi
+    else
+        ICON=""  # Cable
+    fi
+    IP="$ICON $CUR_IP"
+    break  # Solo mostramos la primera activa
+done
+
+
+# Icono y color en Polybar
+echo "%{F#2495e7} %{F#ffffff}$IP%{u-}"
